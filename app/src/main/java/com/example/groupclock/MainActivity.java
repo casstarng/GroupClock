0bb9e867cc40ec913d365d2964a82c51219f2907
HaoboZ <haobozhang9081@gmail.com>
2019-02-21 15:46:35 -0800

    Working timers

--- app/src/main/java/com/example/groupclock/MainActivity.java
+++ app/src/main/java/com/example/groupclock/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.groupclock;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,11 +22,17 @@ public class MainActivity extends AppCompatActivity {
 	
 	private class Value {
 		int id;
-		String value;
+		String name;
+		long time;
+		long saved;
+		boolean active;
 		
-		Value(int id, String value) {
+		Value(int id, String name) {
 			this.id = id;
-			this.value = value;
+			this.name = name;
+			time = 0;
+			saved = 0;
+			active = false;
 		}
 	}
 	
@@ -42,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
 	}
 	
 	private void generateListContent() {
-		for (int i = 0; i < 12; i++) {
+		for (int i = 0; i < 9; i++) {
 			data.put(String.valueOf(i), new Value(i, "List Item " + i));
 		}
 	}
@@ -59,22 +66,48 @@ public class MainActivity extends AppCompatActivity {
 		
 		@Override
 		public View getView(final int position, View convertView, ViewGroup parent) {
-			ViewHolder mainViewholder;
+			final ViewHolder mainViewholder;
 			if (convertView == null) {
 				LayoutInflater inflater = LayoutInflater.from(getContext());
 				convertView = inflater.inflate(layout, parent, false);
 				ViewHolder viewHolder = new ViewHolder();
 				viewHolder.title = convertView.findViewById(R.id.list_item_text);
+				viewHolder.time = convertView.findViewById(R.id.time_view);
 				viewHolder.playBtn = convertView.findViewById(R.id.playBtn);
 				viewHolder.pauseBtn = convertView.findViewById(R.id.pauseBtn);
 				convertView.setTag(viewHolder);
 			}
+			
 			mainViewholder = (ViewHolder) convertView.getTag();
+			mainViewholder.time.setText("0");
+			final Handler h = new Handler();
+			final Runnable r = new Runnable() {
+				
+				public void run() {
+					Value item = getItem(position);
+					mainViewholder.time.setText(String.valueOf(System.currentTimeMillis() - item.time));
+					h.postDelayed(this, 0);
+				}
+				
+			};
+			
 			mainViewholder.playBtn.setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View v) {
 					// Play
 					Log.i("==Clicked==", "Item " + position + " was clicked");
+					
+					Value item = getItem(position);
+					if (item.active) return;
+					
+					h.postDelayed(r, 0);
+					
+					if (item.saved == 0)
+						item.time = System.currentTimeMillis();
+					else
+						item.time = System.currentTimeMillis() - item.saved;
+					item.saved = 0;
+					item.active = true;
 				}
 			});
 			
@@ -83,9 +116,25 @@ public class MainActivity extends AppCompatActivity {
 				public void onClick(View v) {
 					// Pause
 					Log.i("==Clicked==", "Item " + position + " was clicked");
+					
+					Value item = getItem(position);
+					if (!item.active) {
+						item.time = 0;
+						item.saved = 0;
+						mainViewholder.time.setText("0");
+						return;
+					}
+					
+					h.removeCallbacks(r);
+					
+					if (item.time != 0)
+						item.saved = System.currentTimeMillis() - item.time;
+					item.active = false;
+					mainViewholder.time.setText(String.valueOf(item.saved));
 				}
 			});
-			mainViewholder.title.setText(getItem(position).value);
+			Value item = getItem(position);
+			mainViewholder.title.setText(item.name);
 			
 			return convertView;
 		}
@@ -93,6 +142,7 @@ public class MainActivity extends AppCompatActivity {
 	
 	public class ViewHolder {
 		TextView title;
+		TextView time;
 		Button playBtn;
 		Button pauseBtn;
 	}
