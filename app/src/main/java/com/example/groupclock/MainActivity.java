c0ec5fbc87b5bf55a400fc7f26d3fc59ed562ca2
HaoboZ <haobozhang9081@gmail.com>
2019-02-21 15:02:42 -0800

    All list items have separate clickers?

--- app/src/main/java/com/example/groupclock/MainActivity.java
+++ app/src/main/java/com/example/groupclock/MainActivity.java
@@ -1,95 +1,99 @@
 package com.example.groupclock;
 
-import android.app.Activity;
 import android.content.Context;
-import android.os.Handler;
-import android.os.SystemClock;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ArrayList<String> data = new ArrayList<String>();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ListView lv = (ListView) findViewById(R.id.listview);
-        generateListContent();
-        lv.setAdapter(new MyListAdaper(this, R.layout.list_item, data));
-
-    }
-
-    private void generateListContent() {
-        for(int i = 0; i < 12; i++) {
-            data.add("List Item " + i);
-        }
-    }
-
-    private class MyListAdaper extends ArrayAdapter<String> {
-        private int layout;
-        private List<String> mObjects;
-        private MyListAdaper(Context context, int resource, List<String> objects) {
-            super(context, resource, objects);
-            mObjects = objects;
-            layout = resource;
-        }
-
-        @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
-            ViewHolder mainViewholder = null;
-            if(convertView == null) {
-                LayoutInflater inflater = LayoutInflater.from(getContext());
-                convertView = inflater.inflate(layout, parent, false);
-                ViewHolder viewHolder = new ViewHolder();
-                viewHolder.title = (TextView) convertView.findViewById(R.id.list_item_text);
-                viewHolder.playBtn = (Button) convertView.findViewById(R.id.playBtn);
-                viewHolder.pauseBtn = (Button) convertView.findViewById(R.id.pauseBtn);
-                convertView.setTag(viewHolder);
-            }
-            mainViewholder = (ViewHolder) convertView.getTag();
-            mainViewholder.playBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Play
-                    Log.i("==Clicked==", "Item " + position + " was clicked");
-                }
-            });
-
-            mainViewholder.pauseBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Pause
-                    Log.i("==Clicked==", "Item " + position + " was clicked");
-                }
-            });
-            mainViewholder.title.setText(getItem(position));
-
-            return convertView;
-        }
-    }
-    public class ViewHolder {
-        TextView title;
-        Button playBtn;
-        Button pauseBtn;
-    }
+	
+	private class Value {
+		int id;
+		String value;
+		
+		Value(int id, String value) {
+			this.id = id;
+			this.value = value;
+		}
+	}
+	
+	private Map<String, Value> data = new HashMap<>();
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		ListView lv = findViewById(R.id.listview);
+		generateListContent();
+		lv.setAdapter(new MyListAdapter(this, R.layout.list_item, new ArrayList<>(data.values())));
+		
+	}
+	
+	private void generateListContent() {
+		for (int i = 0; i < 12; i++) {
+			data.put(String.valueOf(i), new Value(i, "List Item " + i));
+		}
+	}
+	
+	private class MyListAdapter extends ArrayAdapter<Value> {
+		private int layout;
+		private List<Value> mObjects;
+		
+		private MyListAdapter(Context context, int resource, List<Value> objects) {
+			super(context, resource, objects);
+			mObjects = objects;
+			layout = resource;
+		}
+		
+		@Override
+		public View getView(final int position, View convertView, ViewGroup parent) {
+			ViewHolder mainViewholder;
+			if (convertView == null) {
+				LayoutInflater inflater = LayoutInflater.from(getContext());
+				convertView = inflater.inflate(layout, parent, false);
+				ViewHolder viewHolder = new ViewHolder();
+				viewHolder.title = convertView.findViewById(R.id.list_item_text);
+				viewHolder.playBtn = convertView.findViewById(R.id.playBtn);
+				viewHolder.pauseBtn = convertView.findViewById(R.id.pauseBtn);
+				convertView.setTag(viewHolder);
+			}
+			mainViewholder = (ViewHolder) convertView.getTag();
+			mainViewholder.playBtn.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					// Play
+					Log.i("==Clicked==", "Item " + position + " was clicked");
+				}
+			});
+			
+			mainViewholder.pauseBtn.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					// Pause
+					Log.i("==Clicked==", "Item " + position + " was clicked");
+				}
+			});
+			mainViewholder.title.setText(getItem(position).value);
+			
+			return convertView;
+		}
+	}
+	
+	public class ViewHolder {
+		TextView title;
+		Button playBtn;
+		Button pauseBtn;
+	}
 }
